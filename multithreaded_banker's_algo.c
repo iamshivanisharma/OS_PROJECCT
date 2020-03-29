f36e5a79f59f928de33c0261551b71eba3963b87
Shivani Sharma <47072878+iamshivanisharma@users.noreply.github.com>
2020-03-29 14:33:15 +0530

    Update multithreaded_banker's_algo.c

--- multithreaded_banker's_algo.c
+++ multithreaded_banker's_algo.c
@@ -1,120 +1,191 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <stdbool.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+int r;
+	int Allocation[100][100];
+	int Maximum[100][100];
+	int Need[100][100];
+	int Work[1][100];
+	int n,m,pid,choice,p;
+void show(int x[][100],int n,int m){
+	int i,j;
+	for(i=0;i<n;i++){
+		printf("\n");
+		for(j=0;j<m;j++){
+			printf("%d\t",x[i][j]);
+		}
+	}	
+}
 
-int main()
+//RESOURCE REQUEST ALGORITHM
+void *resource_request()
+{   pid=pid-1;
+	int X[100][100],Y[100][100],Available[100][100],Allocation[100][100];
+	for(int i=0;i<100;i++)
+		for(int j=0;j<100;j++)
+		   {
+			   X[i][j]=Allocation[i][j];
+		   }
+for(int i=0;i<100;i++)
+		for(int j=0;j<100;j++)
+		   {
+			   Y[i][j]=Need[i][j];
+		   }
+		   for(int i=0;i<100;i++)
+			for(int j=0;j<100;j++)
+		   {
+			   Available[i][j]=Work[i][j];
+		   }
+
+	int request[1][100];
+	int i;
+	printf("\n Enter additional request :- \n");
+	for(i=0;i<m;i++){
+		printf(" Request for resource %d : ",i+1);
+		scanf("%d",&request[0][i]);
+	}
+	
+	for(i=0;i<m;i++)
+		if(request[0][i] > Y[pid][i]){
+			printf("\n Error encountered (process has exceeded it's maximum claim ).\n");
+			//break;
+			exit(0);
+	}
+
+	for(i=0;i<m;i++)
+		if(request[0][i] > Available[0][i]){
+			printf("\n Resources unavailable.\n");
+			//break;
+			exit(0);
+		}
+	
+	for(i=0;i<m;i++){
+		Available[0][i]-=request[0][i];
+		X[pid][i]+=request[0][i];
+		Y[pid][i]-=request[0][i];
+	}
+   
+}
+
+//SAFETY ALGORITHM
+int safetyAlgo(int Allocation[][100],int Need[][100],int Available[1][100],int n,int m,int a[]){
+
+	int i,j,k,x=0;
+	int Finish[100],Work[1][100];
+	int flag1=0,flag=0;
+	for(i=0;i<n;i++)
+		Finish[i]=0;
+	for(i=0;i<m;i++)
+		Work[0][i]=Available[0][i];
+
+	for(k=0;k<n;k++){
+		for(i=0;i<n;i++){
+			if(Finish[i] == 0){
+				flag=0;
+				for(j=0;j<m;j++){
+					if(Need[i][j] > Work[0][j])
+						flag=1;
+				}
+				if(flag == 0 && Finish[i] == 0){
+					for(j=0;j<m;j++)
+						Work[0][j]+=Allocation[i][j];
+					Finish[i]=1;
+					flag1++;
+					a[x++]=i;
+				}
+			}
+		}
+		if(flag1 == n)
+			return 1;
+	}
+	return 0;
+}
+
+
+//BANKER'S ALGORITHM
+void get(int Allocation[][100],int Need[][100],int M[100][100],int Work[1][100],int *n,int *m){
+	int i,j;
+	printf("\n Enter total number of processes : ");
+	scanf("%d",n);
+	printf("\n Enter total number of resources : ");
+	scanf("%d",m);
+	for(i=0;i<*n;i++){
+		printf("\n Process %d\n",i+1);
+		for(j=0;j<*m;j++){
+			printf(" Allocation for resource %d : ",j+1);
+			scanf("%d",&Allocation[i][j]);
+			printf(" Maximum for resource %d : ",j+1);
+			scanf("%d",&Maximum[i][j]);
+		}
+	}
+	printf("\n Available resources : \n");
+	for(i=0;i<*m;i++){
+		printf(" Resource %d : ",i+1);
+		scanf("%d",&Work[0][i]);
+	}
+
+	for(i=0;i<*n;i++)
+		for(j=0;j<*m;j++)
+			Need[i][j]=Maximum[i][j]-Allocation[i][j];
+
+	printf("\n Allocation Matrix");
+	show(Allocation,*n,*m);
+	printf("\n Maximum Requirement Matrix");
+	show(Maximum,*n,*m);
+	printf("\n Need Matrix");
+	show(Need,*n,*m);
+
+}
+
+int bankerAlgo(int Allocation[][100],int Need[][100],int Work[1][100],int n,int m){
+	int j,i,a[100];
+	j=safetyAlgo(Allocation,Need,Work,n,m,a);
+	if(j != 0 ){
+		printf("\n\n");
+		for(i=0;i<n;i++)
+		     printf(" P%d  ",a[i]);
+		printf("\n A safety sequence has been detected.\n");
+		return 1;
+	}else{
+		printf("\n Deadlock has occured.\n");
+		return 0;
+		
+	}
+}
+void create_thread(int n)
 {
-
-  int processes=0,resources=0;
-  printf("Enter number of processes: "); 
-  scanf("%d",&processes);
-  printf("Enter number of resources: ");
-  scanf("%d",&resources);
- // int s[processes];
-int f[processes], ans[processes], ind = 0; 
-  
-
-  int allocation[processes][resources];
-  int maximum[processes][resources];
-  int available[resources];
-  int need[processes][resources];
- for(int i=0;i<processes;i++)
-  {
-    printf("Enter the allocated resources: ");
-    for(int j=0;j<resources;j++)
-    {
-       
-        scanf("%d",&allocation[i][j]);
-    }
-   
-  }
-for(int i=0;i<processes;i++)
-  {
-      printf("Enter the maximum  resources which can be provided: ");
-    for(int j=0;j<resources;j++)
-    {
-         
-        scanf("%d",&maximum[i][j]);
-
-    }
-   
-  }
-for(int i=0;i<processes;i++)
-  {
-      printf("\n Number of instances needed: ");
-    for(int j=0;j<resources;j++)
-    {
-        
-        need[i][j] = maximum[i][j] - allocation[i][j];
-       printf(" %d ", need[i][j]);
- 
-    }
-   
-  }
+	for(int i=0;i<n;i++)
+	{	printf("\n Enter process number: ");
+		scanf("%d",&pid);
+		pthread_t pid;
+		pthread_create(&pid,NULL,resource_request,NULL);
+		pthread_join(pid,NULL);
+		int val=bankerAlgo(Allocation,Need,Work,n,m);
+   if(val==0)
+   {
+	   exit(0);
+   }
+	}
 
 
-
-       printf("\n Enter the available resources: ");
-    for(int j=0;j<resources;j++)
-    {
-    
-      scanf("%d",&available[j]);
-    }
-  //**************
+}
 
 
-    int s,i;
-  int  work[processes][resources],finish[processes],flag=1,safe_sequence[processes],index=0;
-    
-for(int i=0;i<processes;i++)
-    {   for(int j=0;j<resources;j++)
-        {work[processes][resources]=available[resources];
-        }
-    }
-    for(int i=0;i<processes;i++)
-    {
-        finish[i]=0;
-    }
+int main(){
 
-    for(int i=0;i<processes;i++)
-
-    {
-        for(int j=0;j<resources;j++)
-        {
-        if(finish[i]==0 && need[i][j]<=work[i][j])
-        { safe_sequence[i++]=i;
-            flag=0;
-          work[i][j]=work[i][j]+allocation[i][j];
-          finish[i]=1;
-	break;
-
-        }
-
-
-
-
-
-        
-        }
-    }
-    if(flag==1)
-    {
-        for(int i=0;i<processes;i++)
-        finish[i]=1;
-        printf("System in safe state\n");
-    }
-
-  
-printf("Safe Sequence as follows");
-
-    for(int i=0;i<processes-1;i++)
-   	 printf(" P%d->",safe_sequence[i] );
-    printf(" P%d", safe_sequence[processes - 1]); 
-
-return (0);
-
-
+	printf("\n MULTITHREADED DEADLOCK AVOIDANCE PROGRAM USING BANKER'S ALGORITHM\n");
+	get(Allocation,Need,Maximum,Work,&n,&m);
+	r=bankerAlgo(Allocation,Need,Work,n,m);
+	if(r !=0 ){
+		printf("\n Do you want make an additional request ? (1=Yes|0=No)");
+		scanf("%d",&choice);
+		if(choice == 1){
+            printf("Enter number of requests: ");
+            scanf("%d",&n);
+			create_thread(n);
+            
+		}
+	}else
+		exit(0);
+	return 0;
 }
